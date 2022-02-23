e909abe48b98eaed54d9cc06d6a6d7dc0ae71958
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-02-23 18:16:52 -0500

    fixed

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -6,3 +6,9 @@ function testFunc()
 {
     alert("hey you tested me");
 }
+function jpDate()
+{
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    document.write("Today is " + time + " on " + today.toDateString());
+}

322c45d03a22694f8d55fb20622d308801752380
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-02-23 23:44:22 -0500

    JS

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,13 +2,39 @@ function scriptTest()
 {
     alert("Hey my script is running");
 }
-function testFunc()
-{
-    alert("hey you tested me");
-}
 function jpDate()
 {
     const today = new Date();
     const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     document.write("Today is " + time + " on " + today.toDateString());
+}
+function displayGreeting()
+{
+    let readName = document.getElementById("inputName").value;
+    let readMood = document.getElementById("inputMood").value;
+    document.getElementById("jpGreeting").innerHTML = (
+        "<p>Greetings " + readName + ", I am glad you are " + readMood + "!"
+    )
+}
+function calculateSeconds()
+{
+    let numString = prompt("Convert a number from minutes to seconds.");
+    let pNum = parseFloat(numString);
+    let seconds = pNum * 60;
+    if (isNaN(pNum))
+    {
+        alert("Not a number!");
+    }
+    else
+    {
+        alert("The amount of seconds in " + pNum + " is " + seconds);
+    }
+}
+function calculateCelsius()
+{
+    let numString = prompt("Convert fahrenheit to celsius");
+    let pNum = parseFloat(numString);
+    let celsius = (pNum - 32) * .5556;
+    celsius = celsius.toFixed(2);
+    alert(pNum + " is " + celsius + " in celsius.");
 }
---
+++ itis3135/scripts/polygon.js
@@ -0,0 +1,59 @@
+function getShape()
+{
+    let numString = prompt("Enter a number 1-10 to find out which polygon correlates with your number.");
+    let pNum = parseInt(numString);
+
+    let polygon = "out of bounds";
+
+    switch (pNum)
+    {
+        case 1:
+            polygon = "henagon";
+            break;
+        case 2:
+            polygon = "digon";
+            break;
+        case 3:
+            polygon = "trigon";
+            break;
+        case 4:
+            polygon = "tetragon";
+            break;
+        case 5:
+            polygon = "pentagon";
+            break;
+        case 6:
+            polygon = "hexagon";
+            break;
+        case 7:
+            polygon = "heptagon";
+            break;
+        case 8:
+            polygon = "octagon";
+            break;
+        case 9:
+            polygon = "enneagon";
+            break;
+        case 10:
+            polygon = "decagon";
+            break;
+    }   
+    return polygon;
+}
+
+function validateEntry() //what is the purpose of this? This could all be done in one function
+{
+    if (htmlShape != "out of bounds")
+    {
+        alert("Your shape is a " + htmlShape)
+    }
+    else 
+    {
+        alert("Your entry is out of bounds");
+    }
+}
+
+function result()
+{
+
+}

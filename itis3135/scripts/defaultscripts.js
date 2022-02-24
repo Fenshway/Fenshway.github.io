5fab08002d474136864a5230d659222dc9823635
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-02-24 16:40:29 -0500

    firstscripts

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -32,9 +32,33 @@ function calculateSeconds()
 }
 function calculateCelsius()
 {
-    let numString = prompt("Convert fahrenheit to celsius");
+    let numString = prompt("Convert your height from inches to centimeters");
     let pNum = parseFloat(numString);
-    let celsius = (pNum - 32) * .5556;
-    celsius = celsius.toFixed(2);
-    alert(pNum + " is " + celsius + " in celsius.");
+    let cm = pNum * 2.54;
+    cm = cm.toFixed(2);
+    alert("Your height is " + cm + "cm.");
+}
+function calculateKMH()
+{
+    let numString = prompt("Convert miles per hour to kilometers per hour");
+    let pNum = parseFloat(numString);
+    let kilometers = pNum * 1.60934;
+    kilometers = kilometers.toFixed(2);
+    alert(pNum + " miles per hour is equal to " + kilometers + " per hour.")
+}
+function calculateKG()
+{
+    let numString = prompt("Convert your weight from lbs to kilograms");
+    let pNum = parseFloat(numString);
+    let kilograms = pNum * .453592;
+    kilograms = kilograms.toFixed(1);
+    alert("Your weight is " + kilograms + " in kilograms.");
+}
+function calculateLitres()
+{
+    let numString = prompt("Convert gallons to litres");
+    let pNum = parseFloat(numString);
+    let litres = gallons * 3.78541;
+    litres = litres.toFixed(2);
+    alert(pNum + " gallons is equal to " + litress + ".");
 }

b80565dd212428b0dc4336f0c200381f6b7a6a1d
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-03-23 22:21:11 -0400

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -4,10 +4,6 @@ let $ = function(id)
 {
     return document.getElementById(id);
 }
-function scriptTest()
-{
-    alert("Hey my script is running");
-}
 function addSalary()
 {
     let salary = $("salary").value;
@@ -17,40 +13,40 @@ function addSalary()
     {
         alert("Submission failed. Salary contained incorrect input")
     }
+    else {
+    salary = salary.toFixed(2);
     salaries.push(salary)
     employees.push(employee)
+    } 
 }
 function displayResults()
 {
     let averageSalary = 0;
     let highestSalary = 0;
-    var personTopSalary;
+    var personTopSalary = "";
     let totalSalary = 0;
     for (let i = 0; i < salaries.length; i++)
     {
-        let resultSalary = salaries[i];
-        totalSalary += resultSalary;
-    }
-    averageSalary = totalSalary/salaries.length;
-    for (let i = 0; i < salaries.length; i++)
-    {
+        let tempSalary = parseFloat(salaries[i]);
+        totalSalary += tempSalary;
+        
         if (salaries[i] > highestSalary)
         {
             personTopSalary = employees[i];
             highestSalary = salaries[i];
         }
     }
-    $('results').innerHTML ="<h2>Results</h2>" + "<p>Average Salary: " + averageSalary + "</p>" + "<p>Highest Earning Employee: " + personTopSalary + " - $" + highestSalary + "</p>";
-
+    averageSalary = (totalSalary/salaries.length);
+    $('results').innerHTML ="<h2>Results</h2>" + "<p>Average Salary: $" + averageSalary.toLocaleString()
+       + "</p>" + "<p>Highest Earning Employee: " + personTopSalary + " - $" + highestSalary.toLocaleString() + "</p>";
 }
 function displaySalary()
 {
-    let createTable ="<table><tr><th>Employee</th><th>Salary</th></tr>";
+    let createTable = "<table><tr><th>Employee</th><th>Salary</th></tr>";
     for(let i = 0; i < employees.length; i++)
     {
-        createTable += "<tr><td>"+ employees[i] + "</td><td>$" + salaries[i].toFixed(2).toLocaleString() + "</td></tr>";
+        createTable += "<tr><td>" + employees[i] + "</td><td>$" + salaries[i].toLocaleString() + "</td></tr>";
     }
     createTable += "</table>";
     $("results_table").innerHTML = createTable;
-
 }

3a3604b8051b91d0bd54f235d5240d27217f21da
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-04-13 19:37:42 -0400

    slideshow

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,42 @@
+const slide = document.querySelector('.slideshow-slide');
+const slideImages = document.querySelectorAll('.slideshow-slide img');
+
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+
+let counter = 1;
+const size = slideImages[0].clientWidth;
+
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextButton.addEventListener('click', function()
+{
+    if (counter >= slideImages.length - 1) return;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter += 1;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevButton.addEventListener('click', function()
+{
+    if (counter <= 0) return;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter -= 1;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+slide.addEventListener('transitionend', function() 
+{
+    if (slideImages[counter].id === 'lastClone')
+    {
+        slide.style.transition = "none";
+        counter = slideImages.length - 2;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (slideImages[counter].id === 'firstClone')
+    {
+        slide.style.transition = "none";
+        counter = slideImages.length - counter;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});

079467cbae2532c67d46123706898d80a87ffb0d
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-04-24 18:39:43 -0400

    ss

--- itis3135/scripts/image-swap.js
+++ itis3135/scripts/image-swap.js
@@ -3,3 +3,11 @@ function swap(element)
     document.getElementById("caption").innerHTML = element.title;
     document.getElementById("image").src = element.src;
 }
+
+/*const test = document.querySelector("#img2");
+img2.addEventListener("click", function()
+{
+    if (counter >= slideImages.length - 1) return;
+    document.getElementById("image").src = (im2.style.transition = "transform 0.4s ease-in-out");
+
+});*/

b27a7dbb66bf8b7b459f1ef873ecf8e71334a643
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-04-24 21:12:23 -0400

    Update slideshow.js

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,15 +1,16 @@
+
+
 const slide = document.querySelector(".slideshow-slide");
 const slideImages = document.querySelectorAll(".slideshow-slide img");
 
 const prevButton = document.querySelector("#prevButton");
 const nextButton = document.querySelector("#nextButton");
 
-/*
 let counter = 1;
 const size = slideImages[0].clientWidth;
 
 slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-*/
+
 nextButton.addEventListener("click", function()
 {
     if (counter >= slideImages.length - 1) return;
@@ -44,14 +45,14 @@ slide.addEventListener('transitionend', function()
 
 function bugFix()
 {
-const slide = document.querySelector(".slideshow-slide");
-const slideImages = document.querySelectorAll(".slideshow-slide img");
+slide = document.querySelector(".slideshow-slide");
+slideImages = document.querySelectorAll(".slideshow-slide img");
 
-const prevButton = document.querySelector("#prevButton");
-const nextButton = document.querySelector("#nextButton");
+prevButton = document.querySelector("#prevButton");
+nextButton = document.querySelector("#nextButton");
 
-let counter = 1;
-const size = slideImages[0].clientWidth;
+counter = 1;
+size = slideImages[0].clientWidth;
 
 slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 }

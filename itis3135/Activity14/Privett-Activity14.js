c96347b2de8165de639133c89cd85340b5d4b58f
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-05-05 22:28:45 -0400

    activity 14

--- itis3135/Activity14/Privett-Activity14.js
+++ itis3135/Activity14/Privett-Activity14.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
     $("#nav_list li").click(function() {
-        $.ajax({
-
-
-        });
-
+        $.ajax( {
+            url: file,
+            dataType: "Text",
+            success: function(file) {
+                let data = $.parseJSON(file);
+                $("main > h1").html(data.speakers[0].title);
+                $("main > h2").html(data.speakers[0].month);
+                $("main > h3").html(data.speakers[0].speaker);
+                $("main > img").attr("src", data.speakers[0].image);
+                $("main > p").html(data.speakers[0].text);
+            }
+        })
     });
-}); // end ready
+});

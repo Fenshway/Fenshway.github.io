1d76827f89fad904b66f3fd6fbd6fa0a08193f83
Justin Privett <81169413+Fenshway@users.noreply.github.com>
2022-05-07 15:09:45 -0400

    header

--- itis3135/project/scripts/jq_plug.js
+++ itis3135/project/scripts/jq_plug.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
         alert("Form Submission Successful!");
     });
 });
-
 $(document).ready(function () {
     $("#contactBtn").click(function () {
         alert("Thank you for reaching out to us. We will return your email as soon as possible");
